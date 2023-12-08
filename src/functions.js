e90f2a5d8eb46636e8a182baeaa9593c68ef1dac
RobertRemind <robert@remindbi.com>
2023-12-09 02:09:34 +1100

    Loading function.js from task pane

--- src/functions.js
+++ src/functions.js
@@ -8,51 +8,10 @@
  * @returns {number} The sum of the two numbers.
  */
 export function add(first, second) {
-  console.log("started", first,second);
+  logMessage("started Add");
   return first + second;
 }
 
-/**
- * Displays the current time once a second
- * @customfunction
- * @param {CustomFunctions.StreamingInvocation<string>} invocation Custom function invocation
- */
-export function clock(invocation) {
-  const timer = setInterval(() => {
-    const time = currentTime();
-    invocation.setResult(time);
-  }, 1000);
-
-  invocation.onCanceled = () => {
-    clearInterval(timer);
-  };
-}
-
-/**
- * Returns the current time
- * @returns {string} String with the current time formatted for the current locale.
- */
-export function currentTime() {
-  return new Date().toLocaleTimeString();
-}
-
-/**
- * Increments a value once a second.
- * @customfunction
- * @param {number} incrementBy Amount to increment
- * @param {CustomFunctions.StreamingInvocation<number>} invocation
- */
-export function increment(incrementBy, invocation) {
-  let result = 0;
-  const timer = setInterval(() => {
-    result += incrementBy;
-    invocation.setResult(result);
-  }, 1000);
-
-  invocation.onCanceled = () => {
-    clearInterval(timer);
-  };
-}
 
 /**
  * Writes a message to console.log().
