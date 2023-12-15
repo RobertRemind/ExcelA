396f5e43f191bc176623c3730884b783bf305bbb
RobertRemind <robert@remindbi.com>
2023-12-15 11:59:44 +1100

    Merged taskpanes and undo of new folder structure.

---
+++ src/functions.js
@@ -0,0 +1,43 @@
+﻿﻿/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+/**
+ * Get value for key
+ * @customfunction
+ * @param key The key
+ * @returns The value for the key.
+ */
+function getValueForKeyCF(key) {
+  debugger;
+  return key;
+}
+
+/**
+ * Get value for key
+ * @customfunction
+ * @param key The key
+ * @returns The value for the key.
+ */
+function setValueForKeyCF(key, value) {
+  setValueForKey(key, value);
+  return "Stored key/value pair";
+}
+
+
+
+/**
+ * Add two numbers
+ * @customfunction
+ * @param key The key
+ * @returns The value for the key.
+ */
+function add(a, b) {
+  debugger;
+  return a + b;
+}
+
+CustomFunctions.associate("GETVALUEFORKEYCF", getValueForKeyCF);
+CustomFunctions.associate("SETVALUEFORKEYCF",setValueForKeyCF);
+CustomFunctions.associate("ADD",add);
