b95ec36a1ab6a3f61a5aabf0b8f1f5e0795f8815
RobertRemind <robert@remindbi.com>
2023-12-24 16:41:09 +1100

    Start Tracked Column management

--- src/taskpanes/refresh/taskpane.js
+++ src/taskpanes/refresh/taskpane.js
@@ -137,15 +137,27 @@ const TrackedTables = {
 		products: {  // Dimension Name
 			name: "ProductsTable",
 			worksheet: "Product",
-			range: "A1:C1",
 			styles: {
 				header: "defaultTableHeader", 
+				tracked: "defaultTableBody",
 				body: "defaultTableBody"
 			}, 
 			trackedColumns: [
-				{name: "Product"}, 
-				{name: "primarySystemCode"}, 
-				{name: "memberCaption"}
+				{
+					colname: "Product", // What is the name of the column in the data table. 
+					source: "Product", // What is the Data source column name?
+					isDirty: false	 // Used to tag that the column has been changed and prompt the user to disable tracking. 
+				}, 
+				{
+					name: "Primary System Code",
+					source: "primarySystemCode", 
+					isDirty: false	
+				}, 
+				{
+					name: "Member Caption",
+					source: "memberCaption", 
+					isDirty: false	
+				}
 			], 
 			rows: []
 		}
@@ -304,6 +316,33 @@ async function setTableStyle(tableName, styleName) {
     });
 }
 
+/**
+ * Do two range strings intersect?
+ * @param {string} range1 Excel range as string
+ * @param {string} range2 Excel range as string
+ * @returns boolean
+ */
+async function isIntersectRange(range1, range2) {
+	Excel.run(async (context) => {
+		const sheet = context.workbook.worksheets.getActiveWorksheet();
+		const intersection = sheet.getRange(`=INTERSECT(${range1}, ${range2})`);
+		intersection.load('address');
+	
+		await context.sync();
+	
+		if (intersection.address) {
+			console.log(`Intersection found at: ${intersection.address}`);
+			return true;
+		} else {
+			console.log("No intersection found.");
+			return false;
+		}
+		
+	}).catch(error => {
+		console.error(error);
+		return false;
+	});
+} 
 
 
 
@@ -1022,13 +1061,16 @@ async function createTrackedTable(context, trackedTable) {
 	tbl.name = trackedTable.name;
 
 	// Bind Change Event
-	tbl.onChanged.add(onTrackedTableChange);
+	tbl.onChanged.add((eventArgs) => {
+        onTrackedTableChange(eventArgs, table);
+    });
 
 	
 	const headerValues = []
 	trackedTable.trackedColumns.map((c) => {
 		headerValues.push(c.name);
 	});
+
 	tbl.getHeaderRowRange().values = [headerValues];
 
 	trackedTable.rows.forEach((r) => {
@@ -1049,9 +1091,56 @@ async function createTrackedTable(context, trackedTable) {
  * Event to watch user updates to Tracked Tables.
  * @param {Excel.TableChangedEventArgs} eventArg 
  */
-async function onTrackedTableChange(eventArg) {
+async function onTrackedTableChange(eventArg, table) {
 	console.log(eventArg);
 	debugger;
+
+	switch (eventArg.changeType) {
+		case "RangeEdited":
+			updateTrackedColumnHeaders(table, eventArg.address)
+
+
+			break;
+		case "RowInserted": 
+		case "RowDeleted":
+		case "ColumnInserted":
+		case "ColumnDeleted":
+		case "CellInserted":
+		case "CellDeleted":
+	}
+
+
+}
+
+/**
+ * Updates the Tracked Table column definition in the event that a user change a Tracked Column name.
+ * @param {Excel.Table} table A Tracked Table.
+ * @param {string} range A string representing the cells address that has been changed 
+ */
+async function updateTrackedColumnHeaders(table, range) {
+	if (isTrackedHeaderIntersect(table, range)) {
+		debugger;
+	} else {
+		console.log("Not a header change.")
+	}
+
+}
+
+
+async function isTrackedHeaderIntersect(table, range){
+	debugger;
+	const header = table.getHeaderRowRange()
+
+	const intersects = isIntersectRange(header.address, range)	
+
+	/*	await Excel.run(async (context) => {
+        
+
+		
+    }).catch(error => {
+        console.error(error);
+    });
+*/
 }
 
 
