41c84ad7588097e5a29b2f887068778fe157c91f
RobertRemind <robert@remindbi.com>
2024-01-06 22:48:43 +1100

    Make SQL

--- src/functions/functions.js
+++ src/functions/functions.js
@@ -44,9 +44,40 @@ function dim(dimension, filters) {
   return dimension
 }
 
+/**
+ * Creates an SQL CREATE TABLE statement.
+ * @customfunction MAKE_SQL
+ * @param {string} tableName Name of the table.
+ * @param {string[][]} columnNames Range of column names.
+ * @param {string[][]} dataTypes Range of data types.
+ * @param {string[][]} precision Range of precision for each data type.
+ * @returns {string} The SQL CREATE TABLE statement.
+ */
+function makeSQL(tableName, columnNames, dataTypes, precision) {
+  let sqlStatement = `CREATE TABLE ${tableName} (`;
+  let columns = [];
+
+  for (let i = 0; i < columnNames.length; i++) {
+      let columnName = columnNames[i][0];
+      let dataType = dataTypes[i][0];
+      let precisionValue = precision[i][0];
+
+      let columnDef = `${columnName} ${dataType}`;
+      if (precisionValue) {
+          columnDef += `(${precisionValue})`;
+      }
+      columns.push(columnDef);
+  }
+
+  sqlStatement += columns.join(', ') + ');';
+  return sqlStatement;
+}
+
+
 
 
 CustomFunctions.associate("GETVALUEFORKEYCF", getValueForKeyCF);
 CustomFunctions.associate("SETVALUEFORKEYCF",setValueForKeyCF);
 CustomFunctions.associate("ADD",add);
 CustomFunctions.associate("DIM",dim);
+CustomFunctions.associate("MAKE_SQL",makeSQL);
