c1521a326b8c6bd909999b37183e9ceeca41de2a
RobertRemind <robert@remindbi.com>
2024-01-06 23:51:08 +1100

    Add back Make SQL

--- src/functions/functions.js
+++ src/functions/functions.js
@@ -34,6 +34,39 @@ function GetValue(key) {
   return OfficeRuntime.storage.getItem(key);
 }
 
+
+/**
+ * Creates an SQL CREATE TABLE statement.
+ * @customfunction MAKE_SQL
+ * @param {string} tableName Name of the table.
+ * @param {string[][]} columnNames Range of column names.
+ * @param {string[][]} dataTypes Range of data types.
+ * @param {string[][]} precision Range of precision for each data type.
+ * @returns {string} The SQL CREATE TABLE statement.
+ */
+function MakeSQL(tableName, columnNames, dataTypes, precision) {
+  let sqlStatement = `CREATE TABLE ${tableName} (`;
+  let columns = [];
+
+  for (let i = 0; i < columnNames.length; i++) {
+      let columnName = columnNames[i][0];
+      let dataType = dataTypes[i][0];
+      let precisionValue = precision[i][0];
+
+      let columnDef = `${columnName} ${dataType}`;
+      if (precisionValue) {
+          columnDef += `(${precisionValue})`;
+      }
+      columns.push(columnDef);
+  }
+
+  sqlStatement += columns.join(', ') + ');';
+  return sqlStatement;
+}
+
+
+
 CustomFunctions.associate("ADD", add);
 CustomFunctions.associate("STOREVALUE",StoreValue);
 CustomFunctions.associate("GETVALUE",GetValue);
+CustomFunctions.associate("MAKESQL",MakeSQL);
